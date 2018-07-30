1bc9d7400f42675ab634f55146a95f5fc344c7a1
Brian Evans <bsevans.unc@gmail.com>
2018-07-29 22:38:56 -0400

    color for biomes and kml

--- raster_to_geotiff.R
+++ raster_to_geotiff.R
@@ -39,13 +39,17 @@ writeRaster(agProj, 'ag.tif', format = 'GTiff', overwrite = TRUE)
 
 KML(agProj, 'agK.kml', col = colorRamps::matlab.like(10), maxpixels = Inf, blur = 10, overwrite = TRUE)
 
-
-# begin to color ----------------------------------------------------------
-
 # Color set for biomes:
 
 bioColors <- c("#9E0142", "#D9444E", "#F77E4A", "#FEC171", "#F3EB91", "#C2E6A0",
                "#79C9A5", "#3D8FBA", "#3D64BA", "#5E4FA2")
+
+KML(raster('biomes.tif'), 'biomes.kml', col = bioColors, maxpixels = Inf, blur = 10, overwrite = TRUE)
+
+
+# begin to color ----------------------------------------------------------
+
+
 
 # Biome colors:
 
